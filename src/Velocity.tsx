03d1daea1466169fdbd3cf12b54bf3fec9449e7e
jshulk <vijaytyagi.vt@gmail.com>
2024-10-14 20:03:12 +0530

    Added velocity chart demo

--- src/Velocity.tsx
+++ src/Velocity.tsx
@@ -1,8 +1,49 @@
+import { useMemo, useState } from "react";
+import CHART_DATA from "./data/chart";
+import "./styles/velocity.css";
 function Velocity() {
+  const [isChartVisible, setChartVisible] = useState(false);
+  const maxHeight = useMemo(() => {
+    return Math.max.apply(
+      null,
+      CHART_DATA.map((data) => data.ticketCount)
+    );
+  }, [CHART_DATA]);
+  function handleChartToggle() {
+    setChartVisible((prev) => !prev);
+  }
+
   return (
     <>
-      <h1>Velocity Page</h1>
+      <div className="velocity-chart-container">
+        <button onClick={handleChartToggle}>Toggle Chart</button>
+        {isChartVisible && (
+          <div className="chart">
+            {CHART_DATA.map((data) => {
+              return <Bar key={data.id} data={data} max={maxHeight} />;
+            })}
+            <label className="y-label">Number of tickets</label>
+            <label className="x-label">Departments</label>
+          </div>
+        )}
+      </div>
     </>
+  );
+}
+
+function Bar({ data, max }: any) {
+  return (
+    <div
+      className="bar"
+      style={{
+        backgroundColor: `${data.colour}`,
+        height: `${(data.ticketCount / max) * 100}%`,
+      }}
+    >
+      <label>
+        {data.name} - {data.ticketCount}
+      </label>
+    </div>
   );
 }
 
