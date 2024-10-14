02fc2e916a96efbae16e8a2a95ce68bfe41f70f3
jshulk <vijaytyagi.vt@gmail.com>
2024-10-14 19:12:51 +0530

    fixed errors

--- src/Velocity.tsx
+++ src/Velocity.tsx
@@ -1,10 +1,4 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-
 function Velocity() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       <h1>Velocity Page</h1>
